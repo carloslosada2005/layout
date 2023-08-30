b2912d94d1f782b42041c824e7c28fb33b0bf5d6
carloslosada2005 <calosadad@libertadores.edu.co>
2023-08-30 16:50:12 -0500

    adicion de una actividad mercancia

---
+++ app/src/main/java/moviles2023/layout/ui/theme/Mercancia.java
@@ -0,0 +1,16 @@
+package moviles2023.layout.ui.theme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import moviles2023.layout.R;
+
+public class Mercancia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mercancia);
+    }
+}
